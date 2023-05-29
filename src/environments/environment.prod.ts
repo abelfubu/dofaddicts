12cadf9277964c548828bcb6d1ff873ba24e5376
Abel de la Fuente <abelfubu@gmail.com>
2023-05-29 17:03:41 +0200

    feat(chore): fix production environment api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl: 'https://dofaddicts.onrender.com',
-  apiUrl: 'https://dofus-db.onrender.com',
+  apiUrl: '/api',
   googleClientId:
     '645975856691-378q5djljiavt4qua50a3p4gqobnaf22.apps.googleusercontent.com',
   authKey: 'df.hv',
